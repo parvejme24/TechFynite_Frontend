cacaabc39613c221b5fe7ea19c6cef95dd23269c
Md Parvej <mdparvejme24@gmail.com>
2025-07-17 23:31:52 +0600

    update firebase config

--- firebase.config.ts
+++ firebase.config.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import { getAnalytics, isSupported } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,5 +19,12 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const auth = getAuth(app);
+
+// Only initialize analytics if running in the browser and supported
+export const analyticsPromise = isSupported().then(supported => {
+  if (supported && typeof window !== "undefined") {
+    return getAnalytics(app);
+  }
+  return null;
+});
