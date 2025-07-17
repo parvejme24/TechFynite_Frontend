b13be8245b227f84fa4790e224646363670fde63
Md Parvej <mdparvejme24@gmail.com>
2025-07-18 00:09:19 +0600

    solve a error

--- components/shared/Navbar/navbar/constants.ts
+++ components/shared/Navbar/navbar/constants.ts
@@ -15,12 +15,12 @@ export const menuVariants = {
   closed: {
     opacity: 0,
     x: "-100%",
-    transition: { duration: 0.3, ease: "easeInOut" },
+    transition: { duration: 0.3, ease: "easeInOut" as const },
   },
   open: {
     opacity: 1,
     x: 0,
-    transition: { duration: 0.3, ease: "easeInOut" },
+    transition: { duration: 0.3, ease: "easeInOut" as const },
   },
 };
 
