2cb2627745ee7cba26b353a56f12a48815d50427
Md Parvej <mdparvejme24@gmail.com>
2025-07-29 15:48:35 +0600

    so7lve template details erro7r

--- app/(WithCommonLayout)/blogs/[id]/page.tsx
+++ app/(WithCommonLayout)/blogs/[id]/page.tsx
@@ -2,10 +2,6 @@ import React from "react";
 import BlogDetailsContainer from "@/components/modules/blogs/BlogDetails/BlogDetailsContainer";
 import Newsletter from "@/components/shared/Newsletter/Newsletter";
 
-interface BlogDetailsPageProps {
-  params: { id: string };
-}
-
 export default function BlogDetailsPage({ params }: { params: { id: string } }) {
   return (
     <div>
