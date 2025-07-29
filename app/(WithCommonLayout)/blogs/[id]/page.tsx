9c93ae7110374d3582d734d9c3fb31d622178fc9
Md Parvej <mdparvejme24@gmail.com>
2025-07-29 15:42:47 +0600

    so7lve page pro7ps related erro7r

--- app/(WithCommonLayout)/blogs/[id]/page.tsx
+++ app/(WithCommonLayout)/blogs/[id]/page.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import BlogDetailsContainer from "@/components/modules/blogs/BlogDetails/BlogDetailsContainer";
 import Newsletter from "@/components/shared/Newsletter/Newsletter";
 
-export default function BlogDetailsPage({
-  params,
-}: {
+interface BlogDetailsPageProps {
   params: { id: string };
-}) {
+}
+
+export default function BlogDetailsPage({ params }: BlogDetailsPageProps) {
   return (
     <div>
       <BlogDetailsContainer id={params.id} />
